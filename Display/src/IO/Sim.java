e253901c6d3bc6e84b1a1995ae73cb43f2903c48
Nathie1000 <NDSchaaphuizen@yahoo.com>
2016-12-16 13:44:37 +0100

    Added SIM class to get CCID.

---
+++ Display/src/IO/Sim.java
@@ -0,0 +1,21 @@
+package IO;
+
+/**
+ * @author Nathan Schaaphuizen
+ *
+ */
+
+/**
+ * @class Sim
+ * Static class to retrieve SIM800C device information.
+ *
+ */
+public class Sim {
+	
+	public static String getCCID(){
+		
+		//Stub code, will be replaced with the real thing later.
+		return "02358494185083431f";
+	}
+
+}
